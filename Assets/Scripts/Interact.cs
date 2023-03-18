19a9222b7ed9ebe3ca03e4f11362c666101a923e
kleberkgns <kleber.silva.kgns@gmail.com>
2023-03-18 16:07:56 -0300

    interact script

---
+++ Assets/Scripts/Interact.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interact : MonoBehaviour
+{ 
+    private MeshRenderer malha;
+    private Color corOriginal;
+
+    void Start()
+    {
+        malha = GetComponent<MeshRenderer>();
+        corOriginal = malha.material.color;
+    }
+
+    public void Selected()
+    {
+        malha.material.color = Color.blue;
+    }
+
+    public void NotSelected()
+    {
+        malha.material.color = corOriginal;
+    }
+}
