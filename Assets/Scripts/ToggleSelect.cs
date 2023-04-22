3b6c1b4fd2f8cce93a15ede3380cd6ce54f82023
kleberkgns <kleber.silva.kgns@gmail.com>
2023-04-22 16:11:49 -0300

    ToggleButton Setup

---
+++ Assets/Scripts/ToggleSelect.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class ToggleSelect : MonoBehaviour
+{
+    public Toggle toggleButton;
+    public Image circleProgress;
+    public float totalTime = 3;
+    private bool gvrStatus;
+    private float gvrTimer;
+    
+    private bool isChecked;
+    
+    public UnityEvent GvrClick;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if(gvrStatus == true)
+        {
+            gvrTimer += Time.deltaTime;
+            circleProgress.fillAmount = gvrTimer / totalTime;
+
+            if(gvrTimer >= totalTime)
+            {
+                GvrClick.Invoke();
+                PlayerPrefs.SetInt("Gun", 1);
+            }
+        }
+        else
+        {
+            gvrTimer = 0;
+            circleProgress.fillAmount = 0;
+        }
+    }
+
+    public void Selected()
+    {
+        gvrStatus = true;
+    }
+
+    public void NotSelected()
+    {
+        gvrStatus = false;
+    }
+
+    
+}
